6172f4bab1c566d0ee8c55f7582a44bd51a999d5
Khushi Pandit <khushiompandit@gmail.com>
2025-06-22 16:47:08 +0530

    feat: API's done

--- Routes/router.js
+++ Routes/router.js
@@ -1,7 +1,19 @@
+const { getOrder, createOrder, getProduct, createProduct, getSupplier, createSupplier, getWalmartStore, createWalmartStore } = require("../Controller");
+
 const router = require("express").Router();
+
 
 router.get("/", (req, res) => {
     res.send("Welcome to the API");
 });
+
+router.get('/v1/api/orders', getOrder);
+router.post('/v1/api/orders', createOrder);
+router.get('/v1/api/products', getProduct);
+router.post('/v1/api/products', createProduct);
+router.get('/v1/api/suppliers', getSupplier);
+router.post('/v1/api/suppliers', createSupplier);
+router.get('/v1/api/walmartStores', getWalmartStore);
+router.post('/v1/api/walmartStores', createWalmartStore);
 
 module.exports = router;
